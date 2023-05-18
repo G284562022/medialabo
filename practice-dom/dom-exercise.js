25bab17f9c025ad5213761fb8b65aacf4a357cf3
G284562022 <228456@st.takushoku-u.ac.jp>
2023-05-18 17:13:47 +0900

    課題

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -23,7 +23,10 @@ p.style.textEmphasis='text-emphasis:sesame green';
 
 
 // 練習4-3 写真表作成プログラム
+let div = document.querySelector('div#pohtotable');
 let p = document.createElement('p');
+p.setAttribute('href', 'practice/taro.png');
+div.insertAdjacentElement('beforeend', p);
 
 
 let p = document.createElement('p');				// 新しい p要素を作成
